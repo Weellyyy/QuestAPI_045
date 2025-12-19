6e9d53dabce76042edd34882da08f9581b7cdfc8
Weellyyy <wildanprtm2304@gmail.com>
2025-12-19 21:39:47 +0700

    menyesuaikan pemanggilan

--- app/src/main/java/com/example/questapi_045/MainActivity.kt
+++ app/src/main/java/com/example/questapi_045/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.questapi_045.theme.QuestAPI_045Theme
+import com.example.questapi_045.uicontroller.DataSiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +21,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestAPI_045Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuestAPI_045Theme {
-        Greeting("Android")
     }
 }
