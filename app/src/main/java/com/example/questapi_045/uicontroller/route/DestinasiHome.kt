4d79eaf19fd6a167daf3691188c7edabc9aa4c9a
Weellyyy <wildanprtm2304@gmail.com>
2025-12-19 21:34:01 +0700

    Kode ini digunakan untuk menentukan rute navigasi dan judul halaman untuk layar home

--- app/src/main/java/com/example/questapi_045/uicontroller/route/DestinasiHome.kt
+++ app/src/main/java/com/example/questapi_045/uicontroller/route/DestinasiHome.kt
@@ -1,4 +1,8 @@
 package com.example.questapi_045.uicontroller.route
 
-object DestinasiHome {
+import com.example.questapi_045.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
 }
