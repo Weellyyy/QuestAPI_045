9e648066e595608298ba8ba49105f01c3ea414a3
Weellyyy <wildanprtm2304@gmail.com>
2025-12-19 19:03:20 +0700

    membuat methode post

--- app/src/main/java/com/example/questapi_045/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_045/apiservice/ServiceApiSiswa.kt
@@ -1,8 +1,13 @@
 package com.example.questapi_045.apiservice
 
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ServiceApiSiswa {
     @GET("bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
+
+    @POST("insertTM.php")
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
 }
