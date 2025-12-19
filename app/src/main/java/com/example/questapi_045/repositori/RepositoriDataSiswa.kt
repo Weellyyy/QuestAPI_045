f93698750af3e3c61796f0f7e2cc25b0da24ff23
Weellyyy <wildanprtm2304@gmail.com>
2025-12-19 19:15:52 +0700

    Menambahkan repository data siswa dengan integrasi API

--- app/src/main/java/com/example/questapi_045/repositori/RepositoriDataSiswa.kt
+++ app/src/main/java/com/example/questapi_045/repositori/RepositoriDataSiswa.kt
@@ -1,2 +1,17 @@
 package com.example.questapi_045.repositori
 
+import com.example.questapi_045.apiservice.ServiceApiSiswa
+import com.example.questapi_045.viewmodel.provider.DataSiswa
+
+interface RepositoryDataSiswa {
+    suspend fun getDataSiswa(): List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void>
+}
+
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+) : RepositoryDataSiswa {
+
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+}
