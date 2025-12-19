23ffbd51149ba266e597f2ec6d0f398c1c60b4d4
Weellyyy <wildanprtm2304@gmail.com>
2025-12-19 21:33:50 +0700

    Kode ini digunakan untuk menentukan rute navigasi dan judul halaman untuk layar tambah data siswa

--- app/src/main/java/com/example/questapi_045/uicontroller/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/questapi_045/uicontroller/route/DestinasiEntry.kt
@@ -1,4 +1,8 @@
 package com.example.questapi_045.uicontroller.route
 
-object DestinasiEntry {
+import com.example.questapi_045.R
+
+object DestinasiEntry : DestinasiNavigasi{
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
 }
