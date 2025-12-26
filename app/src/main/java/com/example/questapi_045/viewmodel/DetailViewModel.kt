37da145c3c005828372976c3332e0503902d719c
Weellyyy <wildanprtm2304@gmail.com>
2025-12-26 14:40:33 +0700

    implementasi DetailViewModel untuk mengambil dan mengelola status detail siswa

--- app/src/main/java/com/example/questapi_045/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_045/viewmodel/DetailViewModel.kt
@@ -1,9 +1,14 @@
 package com.example.questapi_045.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.questapi_045.modeldata.DataSiswa
+import com.example.questapi_045.repositori.RepositoryDataSiswa
 
 sealed interface StatusUIDetail{
     data class Success(val satusiswa: DataSiswa) : StatusUIDetail
     object Error : StatusUIDetail
     object Loading : StatusUIDetail
 }
+class DetailViewModel (savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
+RepositoryDataSiswa): ViewModel()
