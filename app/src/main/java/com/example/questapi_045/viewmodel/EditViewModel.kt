f31b8802cee14c522e6f67a8e2d4a4728baa384c
Weellyyy <wildanprtm2304@gmail.com>
2025-12-26 14:29:24 +0700

    fungsi ini untuk membuat validasi input agar nama, alamat dan telpon tidak kosong

--- app/src/main/java/com/example/questapi_045/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_045/viewmodel/EditViewModel.kt
@@ -31,5 +31,10 @@ RepositoryDataSiswa
         uiStateSiswa =
             UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
     }
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
 
 }
