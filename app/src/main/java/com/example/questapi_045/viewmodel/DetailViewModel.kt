b0f8efe6387e831f859981422c78e973b0919fbe
Weellyyy <wildanprtm2304@gmail.com>
2025-12-26 14:45:05 +0700

    inisialisasi ID siswa dan status UI awal untuk memulai pengambilan data detail siswa secara otomatis

--- app/src/main/java/com/example/questapi_045/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_045/viewmodel/DetailViewModel.kt
@@ -1,5 +1,8 @@
 package com.example.questapi_045.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.example.questapi_045.modeldata.DataSiswa
@@ -11,4 +14,11 @@ sealed interface StatusUIDetail{
     object Loading : StatusUIDetail
 }
 class DetailViewModel (savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
-RepositoryDataSiswa): ViewModel()
+RepositoryDataSiswa): ViewModel(){
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+    init {
+        getSatuSiswa()
+    }
+}
