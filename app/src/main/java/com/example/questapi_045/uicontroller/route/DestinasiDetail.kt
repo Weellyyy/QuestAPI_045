d8d4d1bfbde45fd26ff31685a159f3b3076a7179
Weellyyy <wildanprtm2304@gmail.com>
2025-12-26 15:21:38 +0700

    konfigurasi rute dan argumen navigasi untuk halaman detail siswa

--- app/src/main/java/com/example/questapi_045/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/questapi_045/uicontroller/route/DestinasiDetail.kt
@@ -1,2 +1,11 @@
 package com.example.questapi_045.uicontroller.route
 
+import com.example.questapi_045.R
+
+object DestinasiDetail: DestinasiNavigasi{
+    override val route= "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
